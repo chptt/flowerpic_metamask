00da8618b6605be8844c900d63aefd7018c62a3b
Chandanapt <chandanashajithpt04@gmail.com>
2026-02-16 13:27:00 +0530

    Update all NFT prices to 0.0000001 ETH

--- src/data.js
+++ src/data.js
@@ -2,73 +2,73 @@ export const INITIAL_FLOWERS = [
   {
     id: 1,
     name: "Golden Sunflower Bouquet",
-    price: 0.01,
+    price: 0.0000001,
     image: "/images/flower1.jpg"
   },
   {
     id: 2,
     name: "Midnight Rose Arrangement",
-    price: 0.02,
+    price: 0.0000001,
     image: "/images/flower2.jpg"
   },
   {
     id: 3,
     name: "Spring Tulip Bundle",
-    price: 0.015,
+    price: 0.0000001,
     image: "/images/flower3.jpg"
   },
   {
     id: 5,
     name: "Orchid Whisper",
-    price: 0.03,
+    price: 0.0000001,
     image: "/images/flower5.jpg"
   },
   {
     id: 6,
     name: "Daisy Dream Bouquet",
-    price: 0.008,
+    price: 0.0000001,
     image: "/images/flower6.jpg"
   },
   {
     id: 7,
     name: "Lavender Mist Bundle",
-    price: 0.012,
+    price: 0.0000001,
     image: "/images/flower7.jpg"
   },
   {
     id: 8,
     name: "Cherry Blossom Special",
-    price: 0.025,
+    price: 0.0000001,
     image: "/images/flower8.jpg"
   },
   {
     id: 10,
     name: "Peony Petal Bouquet",
-    price: 0.018,
+    price: 0.0000001,
     image: "/images/flower10.jpg"
   },
   {
     id: 11,
     name: "Blue Hydrangea Collection",
-    price: 0.016,
+    price: 0.0000001,
     image: "/images/flower11.jpg"
   },
   {
     id: 12,
     name: "Poppy Blaze Bundle",
-    price: 0.013,
+    price: 0.0000001,
     image: "/images/flower12.jpg"
   },
   {
     id: 13,
     name: "Jasmine Glow Arrangement",
-    price: 0.014,
+    price: 0.0000001,
     image: "/images/flower13.jpg"
   },
   {
     id: 15,
     name: "Magnolia Bloom Special",
-    price: 0.028,
+    price: 0.0000001,
     image: "/images/flower15.jpg"
   }
 ];
